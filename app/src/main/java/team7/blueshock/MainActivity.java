636eb040db2f95745f5716030cd8b98c9fb0105c
MegaByteMe <code_monkey@comcast.net>
2016-02-23 18:57:26 -0500

    Program Button disabling implemented to ensure pairing occurs first. Stub for DetailsActivity (resulting shock event) implemented.

--- app/src/main/java/team7/blueshock/MainActivity.java
+++ app/src/main/java/team7/blueshock/MainActivity.java
@@ -22,25 +22,31 @@ package team7.blueshock;
         import android.bluetooth.BluetoothManager;
         import android.content.Context;
         import android.content.Intent;
+        import android.content.SharedPreferences;
         import android.content.pm.PackageManager;
         import android.os.Build;
+        import android.support.annotation.Nullable;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.View;
         import android.widget.Button;
         import android.widget.SeekBar;
         import android.widget.TextView;
         import android.widget.Toast;
 
+        import java.util.Map;
+        import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     // Allows UI emulation and testing in android studio
     // true - disables ble/bt checks
     // false - allows ble/bt checks - used for device testing / production release
-    static boolean DEBUG = true;                    // Allows emulation for android studio
+    static boolean DEBUG = true;
 
     private TextView barText;                           // gShock threshold value feedback
-    //private static String ble_not_supported = "Bluetooth Low Energy capability could not be located";
+    private static String ble_not_supported = "Bluetooth Low Energy capability could not be located";
     private static int REQUEST_ENABLE_BT = 1;
 
     @Override
@@ -51,17 +57,17 @@ public class MainActivity extends AppCompatActivity {
         final String ble_not_supported = "Bluetooth Low Energy capability could not be located";
         barText = (TextView) findViewById(R.id.threshText);
         SeekBar sBar = (SeekBar) findViewById(R.id.shockBar); // gShock threshold user input element
-        Button sButt = (Button) findViewById(R.id.button);
 
+        // OS Catch - Ensure minimum OS version that supports BLE
+        if (Build.VERSION.SDK_INT < 18) {
+            // Detect if OS is lower then SDK 18 - first release supporting BLE
+            // If so terminate application - BLE does not exist on this platform
+            // TODO need graceful application termination and notification dialog
+            finish();
+        }
+
+        //Protect against emulator crashes, checking for hardware that doesnt exist
         if(!DEBUG) {
-            // OS Catch - Ensure minimum OS version that supports BLE
-            if (Build.VERSION.SDK_INT < 18) {
-                // Detect if OS is lower then SDK 18 - first release supporting BLE
-                // If so terminate application - BLE does not exist on this platform
-                // TODO need graceful application termination and notification dialog
-                finish();
-            }
-
             // Hardware Catch - Determine if hardware has BLE capability
             if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
                 Toast.makeText(this, ble_not_supported, Toast.LENGTH_SHORT).show();
@@ -73,12 +79,20 @@ public class MainActivity extends AppCompatActivity {
             final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
             BluetoothAdapter myBTAdapter = bluetoothManager.getAdapter();
 
-            // Hardware Catch - Determine if Bluetooth is enabled, if not request enable
+            // If BLE is not enabled, Request Enable
             if (myBTAdapter == null || !myBTAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
         }
+
+        // Disable Program Device button since no BLE devices are paired yet
+        findViewById(R.id.prgBtn).setEnabled(false);
+
+        SharedPreferences prefs =getSharedPreferences("PAIRED", MODE_APPEND);
+        android.content.SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("PAIRED", false);
+        editor.apply();
 
         // UI Operation - Setup listener for user modifying the seek bar
         sBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -98,7 +112,26 @@ public class MainActivity extends AppCompatActivity {
                 //Auto Generated Stub
             }
         });
+    }
 
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("Blue", "onResume");
+
+        SharedPreferences prefs =getSharedPreferences("PAIRED", MODE_APPEND);
+        android.content.SharedPreferences.Editor editor = prefs.edit();
+
+        if(prefs.getBoolean("PAIRED", true)) {
+            findViewById(R.id.prgBtn).setEnabled(true);
+            Log.d("Blue", new String(prefs.getAll().toString()) );
+        }
+        else if(prefs.getBoolean("PAIRED", false)) {
+            findViewById(R.id.prgBtn).setEnabled(false);
+            Log.d("Blue", new String(prefs.toString()) );
+        }
+        else
+            findViewById(R.id.prgBtn).setEnabled(false);
     }
 
     public void prgBtnClick( View V) {
@@ -106,12 +139,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scanBtnClick( View V ) {
-        //startActivity(new Intent(MainActivity.this, scanActivity.class));
-
-        Toast.makeText(this, "you pushed the scan button ", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, scanActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, scanActivity.class));
     }
 
     public void DBGkill( View V ) {
@@ -120,4 +148,15 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    public void DBGdetClick( View V ) {
+        startActivity(new Intent(this, DetailActivity.class));
+    }
+
+    public void DBGclearClick( View V ) {
+        SharedPreferences preferences =getSharedPreferences("PAIRED", MODE_APPEND);
+        android.content.SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+        Log.d("Blue", "Preferences cleared!");
+    }
 }
--- app/src/main/java/team7/blueshock/scanActivity.java
+++ app/src/main/java/team7/blueshock/scanActivity.java
@@ -1,10 +1,14 @@
 package team7.blueshock;
 
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.util.Log;
 
 public class scanActivity extends AppCompatActivity {
 
@@ -12,19 +16,31 @@ public class scanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-
-        Intent intent = getIntent();
-    }
-
-    public void scnBtnClick( View V ) {
-        Toast.makeText(this, "you pushed my button ", Toast.LENGTH_SHORT).show();
     }
 
     public void cancelBtnClick( View V ) {
+        //finish();
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 
     public void selBtnClick( View V ) {
+        // TODO setting pairing in this method is undesirable - this is for UI testing only
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
 
+        SharedPreferences preferences =getSharedPreferences("PAIRED", MainActivity.MODE_APPEND);
+        android.content.SharedPreferences.Editor editor = preferences.edit();
+
+        Log.d("Blue", preferences.getAll().toString());
+
+        editor.putBoolean("PAIRED", true);
+        editor.apply();
+
+        finish();
+    }
+
+    //TODO need discoevery and pairing code
+    public void findBLE() {
     }
 }
